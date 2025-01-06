aacc5d7012954874ee7ad059e54773ff610fd642
sachin27kumar <sachin27sheoran@gmail.com>
2025-01-06 10:37:26 +0530

    Task cards added to homePage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TaskCard from "../components/Taskcard";
+
+const HomePage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">To-Do List</h1>
+      <div className="grid gap-4">
+        {tasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
